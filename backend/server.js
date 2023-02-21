b6f5fbab6961f1c9b35687424c032f3135dde5e7
Houda20-bh <bh.houda14@gmail.com>
2023-02-22 00:31:33 +0100

    rest ApI for goals

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 
 const express=require('express');
 const dotenv= require('dotenv').config();
+const colors= require('colors');
 const { errorHandler}= require('./middleware/erroMiddleware')
+const connectDB= require('./config/db');
 const port= process.env.PORT || 5000;
+connectDB();
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
