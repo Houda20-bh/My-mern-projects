df09f93143f7b8a4ed9453f48939ee3de70e1529
Houda20-bh <bh.houda14@gmail.com>
2023-02-23 04:14:20 +0100

    loolol finish with backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -63,7 +63,12 @@ const loginUser = asyncHandler (async(req,res)=>{
 //@route  GET /api/users/me
 //@access private
 const getMe = asyncHandler (async(req,res)=>{
-    res.json({message:"display User data"})
+  const {_id, name, email} = await myUser.findById(req.user.id);
+  res.status(200).json({
+    id:_id,
+    name,
+    email
+  })
 });
 //Generate JWT
 const generateToken= (id)=>{
